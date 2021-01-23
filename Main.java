0304356cf22f757e16214ddfaa95cd159a1b581a
Emiliano Melendrez <91090413@g.mvusd.net>
2021-01-23 23:44:17 +0000

    Added a panel and 2 buttons

--- Main.java
+++ Main.java
@@ -10,5 +10,14 @@ public class Main
     // You must have this line
     frame1.setSize(500,500);
     frame1.setVisible(true);
+
+    JPanel panel1 = new JPanel(new GridLayout(1, 1, 20, 20));
+
+    //------Button1-------//
+    JButton button1 = new JButton("Button1"); 
+    JButton button2 = new JButton("Button2");
+    panel1.add(button1);
+    panel1.add(button2);
+    frame1.getContentPane().add(BorderLayout.NORTH, panel1);
   }// end of main method
 }// end of class main
